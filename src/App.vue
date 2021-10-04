4fa2ed8e31387182375b7942be2254bdd4ea0032
Jeong Ho <gkood2@gamil.com>
2021-10-04 14:13:40 +0900

    CHORE: eslintrc.json 오탈자 수정

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@
 </template>
 
 <script>
-import Hello from "~/components/Hello"
+import Hello from '~/components/Hello'
 export default {
   components: {
     Hello,
   },
   data() {
     return {
-      msg: "hello Webpack?..",
+      msg: 'hello Webpack?..',
     }
   },
 }
