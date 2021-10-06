eab851073d38116a21fe334f8b32fceaa01fa350
Jeong Ho <gkood2@gamil.com>
2021-10-06 15:19:31 +0900

    Add store

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from 'vuex'
+import message from './mesage'
+
+export default createStore({
+	mudules:{
+		message
+	}
+})
---
+++ src/store/message.js
@@ -0,0 +1,10 @@
+export default {
+	namespaced: true,
+	state() {
+		return{}
+	},
+	getters:{},
+	mutations: {},
+	actions: {}
+}
+//기본적인 store module template
